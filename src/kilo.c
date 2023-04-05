c5480a3b2b6aa36f0a393c89b40999615bfe1ece
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-05 15:51:01 +0200

    Enable raw mode

--- src/kilo.c
+++ src/kilo.c
@@ -1,6 +1,18 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+    struct termios raw;
+
+    // Get terminal attribute
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+    // Set terminal attribute
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
     char c;
     while (read(STDIN_FILENO, &c, 1) ==1 && c != 'q');
     return 0;
