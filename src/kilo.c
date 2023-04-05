72cecaf24b434620bfd0465cd2658f557d89345c
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-05 15:55:46 +0200

    Automatically disable raw mode at exit

--- src/kilo.c
+++ src/kilo.c
@@ -1,11 +1,21 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void enableRawMode() {
-    struct termios raw;
+struct termios orig_termios;
 
-    // Get terminal attribute
-    tcgetattr(STDIN_FILENO, &raw);
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    // Get terminal attribute and store in orig_termios
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    // Call disableRawMode automatically whn the program exits
+    atexit(disableRawMode);
+    
+    // terminal attributes copy before updating them
+    struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO);
     // Set terminal attribute
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
