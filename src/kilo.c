187f1e1379ab20ae1d9af986f42363a41a870b83
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-13 18:46:35 +0200

    Clear terminal before quit

--- src/kilo.c
+++ src/kilo.c
@@ -13,7 +13,7 @@ void die(const char *s) {
     // Clear terminal and reposition the cursor
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
-    
+
     perror(s);
     exit(1);
 }
@@ -90,6 +90,9 @@ void editorProcessKeypress() {
     // CTRL-Q will be used to quit from editor
     switch (c) {
         case CTRL_KEY('q'):
+            // Clear terminal and reposition the cursor
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
