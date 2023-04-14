a3cda400989d643b6af9aa8554c9ad83c5032c5d
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-14 23:04:31 +0200

    Fill also the last line with tilde

--- src/kilo.c
+++ src/kilo.c
@@ -159,7 +159,11 @@ void editorDrawRows() {
     int y;
 
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+
+        if (y < E.screenrows - 1) {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 
