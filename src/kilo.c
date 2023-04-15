b216e1c8d550044930ae5855d8637cc3ef4800e6
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-15 12:38:36 +0200

    Open file and display first line

--- src/kilo.c
+++ src/kilo.c
@@ -176,15 +176,29 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
-void editorOpen() {
-    char *line = "Hello, world!";
-    ssize_t linelen = 13;
+void editorOpen(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        die("editorOpen::fopen");
+    }
 
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    if (linelen != -1) {
+        while (linelen > 0 && (line[linelen-1] == '\n' || line[linelen-1] == '\r')) {
+            linelen--;
+        }
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+
+    free(line);
+    fclose(fp);
 }
 
 int editorReadKey() {
@@ -407,11 +421,14 @@ void initEditor() {
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     enableRawMode();
     initEditor();
-    editorOpen();
-    
+
+    if (argc >= 2) {
+        editorOpen(argv[1]);
+    }
+
     while (1) {
         editorRefreshScreen();
         editorProcessKeypress();
