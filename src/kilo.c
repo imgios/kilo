ea543d006f8497611bc07526dd589469180a1278
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-17 14:01:30 +0200

    Add line number on the right side of status bar

--- src/kilo.c
+++ src/kilo.c
@@ -488,16 +488,23 @@ void editorDrawRows(struct abuf *ab) {
 void editorDrawStatusBar(struct abuf *ab) {
     // Switch to inverted colors: \x1b[7m
     abAppend(ab, "\x1b[7m", 4);
-    char status[80];
+    char status[80], rstatus[80];
     int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+    // Line number
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy +1, E.numrows);
     // Cut string if longer than screen size
     if (len > E.screencols) {
         len = E.screencols;
     }
     abAppend(ab, status, len);
     while (len < E.screencols) {
-        abAppend(ab, " ", 1);
-        len++;
+        if (E.screencols - len == rlen) {
+            abAppend(ab, rstatus, rlen);
+            break;
+        } else {
+            abAppend(ab, " ", 1);
+            len++;
+        }
     }
 
     // Switch back to normal formatting
