19bd6e69aebaf4901d7e47eac552bd441e86a8ac
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-21 18:43:09 +0200

    Adapt editorInsertRow usage

--- src/kilo.c
+++ src/kilo.c
@@ -242,7 +242,7 @@ void editorUpdateRow(erow *row) {
     row->rsize = idx;
 }
 
-void editorInserRow(int at, char *s, size_t len) {
+void editorInsertRow(int at, char *s, size_t len) {
     if (at < 0 || at > E.numrows) {
         return;
     }
@@ -314,7 +314,7 @@ void editorRowDelChar(erow *row, int at) {
 void editorInsertChar(int c) {
     // Check if the cursor is on the tilde line
     if (E.cy == E.numrows) {
-        editorAppendRow("", 0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
@@ -384,7 +384,7 @@ void editorOpen(char *filename) {
         while (linelen > 0 && (line[linelen-1] == '\n' || line[linelen-1] == '\r')) {
             linelen--;
         }
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows, line, linelen);
     }
 
     free(line);
