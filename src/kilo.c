8d6673d19e3b2b6ab2d3e65559ab2c3fa9dac012
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-21 18:59:43 +0200

    Add editor prompt

--- src/kilo.c
+++ src/kilo.c
@@ -73,6 +73,7 @@ struct abuf {
 
 // Prototypes
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
 
 void abAppend(struct abuf *ab, const char *s, int len) {
     // Allocate enough memory to hold the previous string
@@ -494,6 +495,34 @@ int editorReadKey() {
         return '\x1b';
     } else {
         return c;
+    }
+}
+
+char *editorPrompt(char *prompt) {
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1) {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == '\r') {
+            if (buflen != 0) {
+                editorSetStatusMessage("");
+                return buf;
+            }
+        } else if (!iscntrl(c) && c < 128) {
+            if (buflen == bufsize - 1) {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
     }
 }
 
