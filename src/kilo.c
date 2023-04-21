2ca9538271759ce43cfc4eab453410a0b50e259b
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-21 18:47:26 +0200

    Add editorInsertNewline

--- src/kilo.c
+++ src/kilo.c
@@ -320,6 +320,21 @@ void editorInsertChar(int c) {
     E.cx++;
 }
 
+void editorInsertNewline() {
+    if (E.cx == 0) {
+        editorInsertRow(E.cy, "", 0);
+    } else {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
+}
+
 void editorDelChar() {
     // Cursor past the end of the file
     if (E.cy == E.numrows) {
