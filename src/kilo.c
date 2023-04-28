5e0c6b5d4c0d0bff7aa96f54c7796deebb19e7a4
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-28 23:05:16 +0200

    Search highlighting

--- src/kilo.c
+++ src/kilo.c
@@ -37,7 +37,8 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 };
 
 // Data type to store a row of text in our editor
@@ -222,6 +223,8 @@ void editorSyntaxToColor(int hl) {
     switch (hl) {
         case HL_NUMBER:
             return 31;
+        case HL_MATCH:
+            return 34;
         default:
             return 37;
     }
@@ -521,6 +524,8 @@ void editorFindCallback(char *query, int key) {
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
