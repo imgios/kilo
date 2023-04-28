227238e65e8cc5d6c94d6ee6f66af9f2759e4ff4
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-28 17:26:56 +0200

    Syntax highlight refactoring

--- src/kilo.c
+++ src/kilo.c
@@ -208,6 +208,25 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+void editorUpdateSyntax(erow *row) {
+    row->hl = realloc(row->hl, row->rsize);
+    memset(row->hl, HL_NORMAL, row->rsize);
+
+    int i;
+    for (i = 0; i < row->rsize; i++) {
+        row->hl[i] = HL_NUMBER;
+    }
+}
+
+void editorSyntaxToColor(int hl) {
+    switch (hl) {
+        case HL_NUMBER:
+            return 31;
+        default:
+            return 37;
+    }
+}
+
 int editorRowCxToRx(erow *row, int cx) {
     int rx = 0;
     int j = 0;
@@ -264,6 +283,8 @@ void editorUpdateRow(erow *row) {
     // idx now contains the # of chars we copied into row->render
     row->render[idx] = '\0';
     row->rsize = idx;
+
+    editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len) {
@@ -812,16 +833,28 @@ void editorDrawRows(struct abuf *ab) {
             }
             
             char *c = &E.row[filerow].render[E.coloff];
+            unsigned char *hl = &E.row[filerow].hl[E.coloff];
+            int current_color = -1;
             int j;
             for (j = 0; j < len; j++) {
-                if (isdigit(c[j])) {
-                    abAppend(ab, "\x1b[31m", 5);
+                if (hl[j] == HL_NORMAL) {
+                    if (current_color != -1) {
+                        abAppend(ab, "\x1b[39m", 5);
+                        current_color = -1;
+                    }
                     abAppend(ab, &c[j], 1);
-                    abAppend(ab, "\x1b[39m", 5);
                 } else {
+                    int color = editorSyntaxToColor(hl[j]);
+                    if (color != current_color) {
+                        current_color = color;
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+                        abAppend(ab, buf, clen);
+                    }
                     abAppend(ab, &c[j], 1);
                 }
             }
+            abAppend(ab, "\x1b[39m", 5);
         }
 
         abAppend(ab, "\x1b[K", 3);
