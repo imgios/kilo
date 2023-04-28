b1959e06bb7fb576f67d65f75f11e17877c4b8d5
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-28 16:57:44 +0200

    Null callback

--- src/kilo.c
+++ src/kilo.c
@@ -427,7 +427,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
         if (E.filename == NULL) {
             editorSetStatusMessage("Save aborted!");
             return;
@@ -456,7 +456,7 @@ void editorSave() {
 }
 
 void editorFind() {
-    char *query = editorPrompt("Search: %s (ESC to cancel)");
+    char *query = editorPrompt("Search: %s (ESC to cancel)", NULL);
     if (query == NULL) { // Search aborted
         return;
     }
