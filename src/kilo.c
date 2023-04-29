25392c6e0ca7cc2f101231bdb35eda3ee0b2faf8
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-04-29 09:33:20 +0200

    Fix non-printable chars color

--- src/kilo.c
+++ src/kilo.c
@@ -1015,7 +1015,20 @@ void editorDrawRows(struct abuf *ab) {
             int current_color = -1;
             int j;
             for (j = 0; j < len; j++) {
-                if (hl[j] == HL_NORMAL) {
+                // Non-printable chars
+                if (iscntrl(c[j])) {
+                    // Capital letters in ASCII comes after the @
+                    // so we will add its value to @
+                    char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+                    abAppend(ab, "\x1b[7m", 4);
+                    abAppend(ab, &sym, 1);
+                    abAppend(ab, "\x1b[m", 3);
+                    if (current_color != -1) {
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", current_color);
+                        abAppend(ab, buf, clen);
+                    }
+                } else if (hl[j] == HL_NORMAL) {
                     if (current_color != -1) {
                         abAppend(ab, "\x1b[39m", 5);
                         current_color = -1;
